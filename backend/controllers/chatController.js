974b45693b68d9c72a0f47677d75956f4cd73b44
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2024-03-02 17:26:48 +0600

    Refactor chat models

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,103 +1,136 @@
-const { supabase } = require("../config/supabaseConfig");
+const { supabase } = require('../config/supabaseConfig');
 
-const dbChat = require("../models/Chat");
-const dbUser = require("../models/User");
+const dbChat = require('../models/Chat');
+const dbUser = require('../models/User');
 
 const chatController = {
-  getChats: async (req, res) => {
-    try {
-      // Ensure the user is authenticated
-      if (!req.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-      }
+    getChats: async (req, res) => {
+        try {
+            // Ensure the user is authenticated
+            if (!req.user) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
 
-      // Extract the userId of the chat partner from the request parameters
-      const userId = req.params.userId;
+            // Extract the userId of the chat partner from the request parameters
+            const userId = req.params.userId;
+            const limit = parseInt(req.query.limit) || 10;
+            const offset = parseInt(req.query.offset) || 0;
 
-      const { data: chats, error } = await dbChat.getChats(req.user.id, userId);
+            if (!req.query.beforeTime) {
+                const currentDate = new Date();
+                req.query.beforeTime = currentDate.getTime();
+            }
 
-      // Check for any errors from the chat service
-      if (error) {
-        console.error("Error fetching chats:", error);
-        return res.status(500).json({ error: "Failed to fetch chats" });
-      }
+            const { data: chats } = await dbChat.getChats(
+                req.user.id,
+                userId,
+                req.query.beforeTime,
+                limit,
+                offset
+            );
 
-      // If there are no errors, send the retrieved chat messages to the client
-      return res.status(200).json(chats);
-    } catch (error) {
-      // Log the error and return a 500 Internal Server Error status code
-      console.error("Server error when fetching chats:", error.message);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-  },
+            const data = [];
+            for (let chat of chats) {
+                data.push({
+                    createdAt: chat.created_at,
+                    content: chat.content,
+                    senderId: chat.sender_id,
+                    receiverId: chat.receiver_id,
+                });
+            }
 
-  sendMessage: async (req, res) => {
-    try {
-      // Ensure the user is authenticated
-      if (!req.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-      }
+            // If there are no errors, send the retrieved chat messages to the client
+            return res.status(200).json(data);
+        } catch (error) {
+            // Log the error and return a 500 Internal Server Error status code
+            console.error('Server error when fetching chats:', error.message);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
 
-      // Extract the userId of the chat partner from the request parameters
-      const userId = req.params.userId;
+    sendMessage: async (req, res) => {
+        try {
+            // Ensure the user is authenticated
+            if (!req.user) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
 
-      // Extract the message content from the request body
-      const { message } = req.body;
+            // Extract the userId of the chat partner from the request parameters
+            const userId = req.params.userId;
 
-      // Send the message to the chat service
-      const { data, error } = await dbChat.sendMessage(
-        req.user.id,
-        userId,
-        message
-      );
+            // Extract the message content from the request body
+            const { message } = req.body;
+            if (!message) {
+                return res.status(400).json({ error: 'Empty message' });
+            }
 
-      // Check for any errors from the chat service
-      if (error) {
-        console.error("Error sending message:", error);
-        return res.status(500).json({ error: "Failed to send message" });
-      }
+            // Send the message to the chat service
+            const { data } = await dbChat.sendMessage(
+                req.user.id,
+                userId,
+                message
+            );
 
-      // If there are no errors, send the message to the client
-      return res.status(201).json(data);
-    } catch (error) {
-      // Log the error and return a 500 Internal Server Error status code
-      console.error("Server error when sending message:", error.message);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-  },
+            if (!data) {
+                return res
+                    .status(500)
+                    .json({ error: 'Failed to send message' });
+            }
 
-  getChatUsers: async (req, res) => {
-    try {
-      // Ensure the user is authenticated
-      if (!req.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-      }
+            // If there are no errors, send the message to the client
+            return res
+                .status(201)
+                .json({ message: 'Message sent successfully' });
+        } catch (error) {
+            // Log the error and return a 500 Internal Server Error status code
+            console.error('Server error when sending message:', error.message);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
 
-      // Retrieve the list of users with whom the current user has chatted
-      const { userIds } = await dbChat.getChatUsers(req.user.id);
+    getChatUsers: async (req, res) => {
+        try {
+            // Ensure the user is authenticated
+            if (!req.user) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
 
-      // Check for any errors from the chat service
-      if (error) {
-        console.error("Error fetching chat users:", error);
-        return res.status(500).json({ error: "Failed to fetch chat users" });
-      }
+            if (!req.query.beforeTime) {
+                const currentDate = new Date();
+                req.query.beforeTime = currentDate.getTime();
+            }
+            req.query.limit = parseInt(req.query.limit) || 10;
+            req.query.offset = parseInt(req.query.offset) || 0;
 
-      console.log("userIds ", userIds);
+            // Retrieve the list of users with whom the current user has chatted
+            const data = await dbChat.getChatUsers(
+                req.user.id,
+                req.query.beforeTime,
+                req.query.limit,
+                req.query.offset
+            );
 
-      // Fetch details for all users in parallel
-      const userPromises = userIds.map((userId) =>
-        dbUser.fetchUserById(userId)
-      );
-      const users = await Promise.all(userPromises);
+            const response = [];
+            for (let user of data) {
+                response.push({
+                    userId: user.user_id,
+                    username: user.username,
+                    imageUrl: user.profile_url,
+                    lastConversationAt: user.last_conversation_at,
+                });
+            }
 
-      // Send the list of chat users to the client
-      return res.status(200).json(users);
-    } catch (error) {
-      // Log the error and return a 500 Internal Server Error status code
-      console.error("Server error when fetching chat users:", error.message);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-  },
+            // Send the list of chat users to the client
+            return res.status(200).json(response);
+        } catch (error) {
+            // Log the error and return a 500 Internal Server Error status code
+            console.error(
+                'Server error when fetching chat users:',
+                error.message
+            );
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
 };
 
 module.exports = chatController;
--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,18 +1,20 @@
-const supabase = require("../config/supabaseConfig");
+const supabase = require('../config/supabaseConfig');
 
 // Function to retrieve chat messages between two users
-const getChats = async (senderId, receiverId, limit, offset) => {
+const getChats = async (senderId, receiverId, timestamp, limit, offset) => {
     const { data, error } = await supabase
-      .from("chat")
-      .select("*")
-      // Attempt to fetch messages where either condition is met
-      .or(`sender_id.eq.${senderId},receiver_id.eq.${senderId}`)
-      .or(`sender_id.eq.${receiverId},receiver_id.eq.${receiverId}`)
-      .order("created_at", { ascending: false }).range(offset, offset+limit-1);
+        .from('chat')
+        .select('*')
+        // Attempt to fetch messages where either condition is met
+        .or(`sender_id.eq.${senderId},receiver_id.eq.${senderId}`)
+        .or(`sender_id.eq.${receiverId},receiver_id.eq.${receiverId}`)
+        .lt('created_at', timestamp)
+        .order('created_at', { ascending: false })
+        .range(offset, offset + limit - 1);
 
     if (error) {
-      console.error('Error fetching chats:', error.message);
-      throw error;
+        console.error('Error fetching chats:', error.message);
+        throw error;
     }
 
     // Filter the results to only include messages between the two users
@@ -29,45 +31,39 @@ const getChats = async (senderId, receiverId, limit, offset) => {
 // Function to send a message from one user to another
 const sendMessage = async (senderId, receiverId, content) => {
     const { data, error } = await supabase
-      .from("chat")
-      .insert([
-        { sender_id: senderId, receiver_id: receiverId, content: content },
-      ]).select('id');
+        .from('chat')
+        .insert([
+            { sender_id: senderId, receiver_id: receiverId, content: content },
+        ])
+        .select('id');
 
     if (error) {
-      console.error('Error sending message:', error.message);
-      throw error;
+        console.error('Error sending message:', error.message);
+        throw error;
     }
 
     return { data: data[0] }; // Assuming the insert operation returns the inserted row
 };
 
 // Function to retrieve the users with whom the current user has chatted
-const getChatUsers = async (userId, limit, offset) => {
-        const { data, error } = await supabase
-            .from('chat')
-            .select('sender_id, receiver_id')
-            .or(`sender_id.eq.${userId},receiver_id.eq.${userId}`);
+const getChatUsers = async (userId, timestamp, limit, offset) => {
+    const { data, error } = await supabase.rpc('fetch_chat_history', {
+        my_user_id: userId,
+        before_timestamp: timestamp,
+        limit_val: limit,
+        offset_val: offset,
+    });
 
-        if (error) {
-          console.error('Error fetching other chat users', error);
-          throw error;
-        }
+    if (error) {
+        console.error('Error fetching chat partners', error);
+        throw error;
+    }
 
-        // Extract unique user IDs, excluding the current user's ID
-        let userIds = new Set();
-        data.forEach(row => {
-            if (row.sender_id !== userId) userIds.add(row.sender_id);
-            if (row.receiver_id !== userId) userIds.add(row.receiver_id);
-        });
-
-        console.log('userIds:', userIds);
-
-        return { userIds: Array.from(userIds) };
+    return data;
 };
 
 module.exports = {
-  getChats,
-  sendMessage,
-  getChatUsers,
+    getChats,
+    sendMessage,
+    getChatUsers,
 };
